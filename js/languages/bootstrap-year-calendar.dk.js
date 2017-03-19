96a2e769fb1663c1f018f6cf83c39e884b9a64ee
Kristian Schneider <khs@thamesconsulting.com>
2017-03-19 12:53:53 +0100

    Danish Translation

---
+++ js/languages/bootstrap-year-calendar.dk.js
@@ -0,0 +1,28 @@
+﻿/**
+ * Danish translation for bootstrap-year-calendar
+ * Kristian Højklint Schneider
+ */
+
+; (function ($) {
+    $.fn.calendar.dates['nl'] = {
+        days: ["zondag", "maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag"],
+        daysShort: ["zo", "ma", "di", "wo", "do", "vr", "za"],
+        daysMin: ["zo", "ma", "di", "wo", "do", "vr", "za"],
+        months: ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"],
+        monthsShort: ["jan", "feb", "mrt", "apr", "mei", "jun", "jul", "aug", "sep", "okt", "nov", "dec"],
+        weekShort: 'w',
+        weekStart: 1
+    };
+}(jQuery));
+
+; (function ($) {
+    $.fn.calendar.dates['dk'] = {
+        days: ["Søndag", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag"],
+        daysShort: ["sø", "ma", "ti", "on", "to", "fr", "lø"],
+        daysMin: ["sø", "ma", "ti", "on", "to", "fr", "lø"],
+        months: ["januar", "februar", "marts", "april", "maj", "juni", "juli", "august", "september", "oktober", "november", "december"],
+        monthsShort: ["jan", "feb", "mar", "apr", "maj", "jun", "jul", "aug", "sep", "okt", "nov", "dec"],
+        weekShort: 'u',
+        weekStart: 1
+    };
+}(jQuery));
